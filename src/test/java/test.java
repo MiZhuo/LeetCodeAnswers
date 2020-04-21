843791f5013759def9ccbbffd278f18c814672a5
mizhuo <wulibin0809@gmail.com>
2020-04-21 15:57:50 +0800

    DivisorGame

---
+++ src/test/java/test.java
@@ -0,0 +1,16 @@
+import dailyquestion.MyAtoi;
+import org.junit.Test;
+
+/**
+ * @author: Mizhuo
+ * @time: 2020/4/3 5:25 下午
+ * @description:
+ */
+
+public class test {
+    @Test
+    public void test(){
+        MyAtoi atoi = new MyAtoi();
+        atoi.myAtoi("42");
+    }
+}
